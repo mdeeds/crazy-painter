814c544c21eae1ce9c8aaef37c8fbf5a619e5024
Matt Deeds <mdeeds@gmail.com>
2021-11-08 14:22:31 -0800

    A few more levels

--- ts/levelSource.ts
+++ ts/levelSource.ts
@@ -6,13 +6,20 @@ export class LevelSource {
   }
 
   getLevelSpec(levelNumber: number): LevelSpec {
-    switch (levelNumber % 6) {
+    switch (levelNumber % 8) {
       case 0: return new SmallLevel();
       case 1: return new LargeLevel();
       case 2: return new PatternLevel(12, [[1, 2]]);
       case 3: return new PatternLevel(12, [[1], [2]]);
       case 4: return new PatternLevel(15, [[1, 1, 1], [1, 2, 1], [1, 1, 1]]);
       case 5: return new PatternLevel(15, [[1, 2, 1, 2, 1]]);
+      case 6: return new PatternLevel(21, [[1], [1], [1], [2], [1], [1], [1]]);
+      case 7: return new PatternLevel(5, [
+        [1, 2, 1, 2, 1],
+        [2, 1, 2, 1, 2],
+        [1, 2, 1, 2, 1],
+        [2, 1, 2, 1, 2],
+        [1, 2, 1, 2, 1]]);
     }
   }
 
